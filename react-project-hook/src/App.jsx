2fc938be369499b68d6e2a9cabee22623c71144e
KrishnaAsati16 <krishnaasati2006@gmail.com>
2026-05-17 16:30:20 +0530

    first commit

---
+++ react-project-hook/src/App.jsx
@@ -0,0 +1,25 @@
+// import './App.css'
+
+// function App() {
+ 
+//   return (
+//    <div>
+//       <Counter/>
+//    </div>
+//   )
+// }
+
+// export default App
+
+import './App.css'
+import Counter from './components/Counter'  // Counter.jsx file se import
+
+function App() {
+  return (
+    <div>
+     <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ react-project-hook/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Counter.css'
+
+const Counter = () => {
+    const [count,setCount] = useState(0);  // state variable
+  return (
+    <div className="counter-container">
+           <p id="para">you have clicked {count} times</p>
+           <button id="button" onClick={()=>{setCount(count+1)}}>click me</button>
+    </div>
+  )
+}
+
+export default Counter
